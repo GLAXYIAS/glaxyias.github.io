72598399d9faad2d1a40d137f0b6fb9f809fcee3
Glaeesas <Sparefornow2026@outlook.com>
2026-05-06 16:38:54 -0400

    Add cloak database and applyCloak function

--- Cloaks/Cloaks.js
+++ Cloaks/Cloaks.js
@@ -1,1 +1,36 @@
+// This file acts as your cloak database
+export const cloaks = [
+  {
+    name: "Google Classroom",
+    title: "Classes",
+    icon: "https://ssl.gstatic.com/classroom/favicon.png"
+  },
+  {
+    name: "Google Drive",
+    title: "My Drive - Google Drive",
+    icon: "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png"
+  },
+  {
+    name: "Clever",
+    title: "Clever | Portal",
+    icon: "https://assets.clever.com/launchpad/902161f3a/favicon.ico"
+  }
+];
 
+// Function to actually change the tab
+export function applyCloak(cloakName) {
+  const selected = cloaks.find(c => c.name === cloakName);
+  if (selected) {
+    document.title = selected.title;
+    
+    // Change the favicon
+    let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = selected.icon;
+    document.getElementsByTagName('head')[0].appendChild(link);
+    
+    // Save to memory
+    localStorage.setItem('savedCloak', cloakName);
+  }
+}
