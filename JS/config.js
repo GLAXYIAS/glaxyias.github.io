cb63f688a1beda4d97059d8d846dfdbbad8fe0fc
Glaeesas <Sparefornow2026@outlook.com>
2026-05-07 15:15:07 -0400

    Remove games export and getMostPopular function

--- JS/config.js
+++ JS/config.js
@@ -1,14 +1,1 @@
-export const games = [
-  {
-    id: "slope",
-    title: "Slope",
-    url: "Games/slope/index.html",
-    desc: "A fast-paced 3D platformer. Avoid obstacles and stay on the track as you accelerate down the neon slope!",
-    tags: ["3D", "Action", "Runner"],
-    popular: true
-  }
-];
-
-export function getMostPopular() {
-    return games.filter(game => game.popular);
-}
+<h1>Not needed<h1>
